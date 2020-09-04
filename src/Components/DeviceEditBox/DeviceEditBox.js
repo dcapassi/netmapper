2f63a3112e71811d673e99ea5794c4f004d0c7ec
Diego Capassi <diego.capassi.moreira@gmail.com>
2020-09-04 20:59:54 -0300

    Refactored the DeviceEditBox

--- src/Components/DeviceEditBox/DeviceEditBox.js
+++ src/Components/DeviceEditBox/DeviceEditBox.js
@@ -1,29 +1,50 @@
-import React, { useState, useEffect } from "react";
-
-import { Container, Header, Body } from "./styles";
-import { AiOutlineClose } from "react-icons/ai";
-
+import React, { useEffect } from "react";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import Dialog from "@material-ui/core/Dialog";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import Typography from "@material-ui/core/Typography";
 import { deviceList } from "../../Data/DeviceList";
+import Input from "@material-ui/core/Input";
+import InputLabel from "@material-ui/core/InputLabel";
+import Select from "@material-ui/core/Select";
+import ListItemText from "@material-ui/core/ListItemText";
+import MenuItem from "@material-ui/core/MenuItem";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import IconButton from "@material-ui/core/IconButton";
+import CloseIcon from "@material-ui/icons/Close";
+import { makeStyles } from "@material-ui/core/styles";
 
-function DeviceEditBox(props) {
-  const [inputName, setInputName] = useState(props.editElement.elementName);
-  const [customer, setCustomer] = useState();
-  const [model, setModel] = useState();
-  const [accessSwitch, setAccessSwitch] = useState();
-  const [selectedChannel, setSelectedChannel] = useState();
-  const [selectedChannel5Ghz, setSelectedChannel5Ghz] = useState();
+const useStyles = makeStyles((theme) => ({
+  appBar: {
+    position: "relative",
+  },
+  title: {
+    marginLeft: theme.spacing(2),
+    flex: 1,
+  },
+}));
 
-  const [selectedModel, setSelectedModel] = useState();
-  const [selectedSwitch, setSelectedSwitch] = useState();
+export default function FormDialog(props) {
+  const classes = useStyles();
+  const [update, setUpdate] = React.useState({ udpate: false });
+  const [open, setOpen] = React.useState(true);
+  const [name, setName] = React.useState(props.editElement.elementName);
+  const [customer, setCustomer] = React.useState("");
+  const [model, setModel] = React.useState("");
+  const [accessSwitch, setAccessSwitch] = React.useState("");
+  const [selectedModel, setSelectedModel] = React.useState();
 
   //Push a "" item as the first item
   let deviceArray = [];
   deviceArray.push("");
   deviceList.map((entry) => deviceArray.push(entry.model));
 
-  const [modelList, setModelList] = useState(deviceArray);
-  const [ch24GhzList, setCh24Ghz] = useState(["", "1", "6", "11"]);
-  const [ch5GhzList, setCh5GhzList] = useState([
+  const [modelList, setModelList] = React.useState(deviceArray);
+  const [ch24GhzList, setCh24GhzList] = React.useState(["", "1", "6", "11"]);
+  const [ch5GhzList, setCh5GhzList] = React.useState([
     "",
     34,
     36,
@@ -49,8 +70,27 @@ function DeviceEditBox(props) {
     136,
     140,
   ]);
-  const [channel24G, setChannel24G] = useState();
-  const [channel5G, setChannel5G] = useState();
+  const [channel24G, setChannel24G] = React.useState("");
+  const [channel5G, setChannel5G] = React.useState("");
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleSubmit = () => {
+    setOpen(false);
+    setUpdate({ ...update, update: true });
+
+    props.setEditField({
+      targetElement: props.editElement.elementKey,
+      apName: name,
+      model,
+      accessSwitch,
+      customer,
+      channel24G,
+      channel5G,
+    });
+    props.closeBox();
+  };
 
   useEffect(() => {
     props.editType === "ap" &&
@@ -60,151 +100,126 @@ function DeviceEditBox(props) {
           setChannel5G(entry.channel5Ghz);
           setModel(entry.model);
           setCustomer(entry.customer);
-          setSelectedModel(entry.model);
           setAccessSwitch(entry.accessSwitch);
         }
       });
   }, []);
 
   return (
-    <Container
-      editElement={props.editElement}
-      setEditField={props.setEditField}
-    >
-      <Header>
-        <div>
-          <p>Edit - {props.editElement.elementName}</p>
-          <div>
-            <AiOutlineClose
-              onClick={() => {
-                props.closeBox();
-              }}
-            />
-          </div>
-        </div>
-      </Header>
-      {props.editType === "ap" && (
-        <Body>
-          <div>
-            <p>Name</p>
-            <input
-              defaultValue={inputName}
-              onChange={(e) => {
-                setInputName(e.target.value);
-              }}
-              type="text"
-            />
-          </div>
-          <div>
-            <p>Model</p>
-            <select
-              defaultValue={model}
-              value={model}
-              onChange={(e) => {
-                setModel(e.target.value);
-              }}
-              type="text"
+    <>
+      <Dialog
+        fullScreen
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="form-dialog-title"
+      >
+        <AppBar className={classes.appBar}>
+          <Toolbar>
+            <IconButton
+              edge="start"
+              color="inherit"
+              onClick={handleClose}
+              aria-label="close"
             >
-              {deviceArray.map((entry) => {
-                return (
-                  <option key={entry} value={entry}>
-                    {entry}
-                  </option>
-                );
-              })}
-            </select>
-          </div>
-          <div>
-            <p>Customer</p>
-            <input
-              defaultValue={customer}
-              onChange={(e) => {
-                setCustomer(e.target.value);
-              }}
-              type="text"
-            />
-          </div>
-          <div>
-            <p>Channel 2.4 GHz</p>
-            <select
-              defaultValue={channel24G}
-              value={channel24G}
-              onChange={(e) => {
-                setChannel24G(e.target.value);
-              }}
-              name="ch24"
-            >
-              {ch24GhzList.map((entry) => {
-                return (
-                  <option key={entry} value={entry}>
-                    {entry}
-                  </option>
-                );
-              })}
-            </select>
-          </div>
-          <div>
-            <p>Channel 5 GHz</p>
-            <select
-              defaultValue={channel5G}
-              value={channel5G}
-              onChange={(e) => {
-                setChannel5G(e.target.value);
-              }}
-              name="ch5"
-            >
-              {ch5GhzList.map((entry) => {
-                return (
-                  <option key={entry} value={entry}>
-                    {entry}
-                  </option>
-                );
-              })}
-            </select>
-          </div>
-          <div>
-            <p>Access Switch</p>
-            <select
-              defaultValue={accessSwitch}
-              value={accessSwitch}
-              onChange={(e) => {
-                setAccessSwitch(e.target.value);
-              }}
-              name="Access Switch"
-            >
-              <option></option>
-              {props.switchList.map((entry) => {
-                return (
-                  <option key={entry.switchName} value={entry.switchName}>
-                    {entry.switchName}
-                  </option>
-                );
-              })}
-            </select>
-          </div>
-          <div style={{ display: "flex", justifyContent: "center" }}>
-            <button
-              onClick={(e) => {
-                props.setEditField({
-                  targetElement: props.editElement.elementKey,
-                  apName: inputName,
-                  model,
-                  accessSwitch,
-                  customer,
-                  channel24G,
-                  channel5G,
-                });
-                props.closeBox();
-                e.stopPropagation();
-              }}
-            >
-              Confirm
-            </button>
-          </div>
-        </Body>
-      )}
-    </Container>
+              <CloseIcon />
+            </IconButton>
+            <Typography variant="h6" className={classes.title}>
+              {`Edit  - ${props.editElement.elementName}`}
+            </Typography>
+            <Button autoFocus color="inherit" onClick={handleSubmit}>
+              save
+            </Button>
+          </Toolbar>
+        </AppBar>
+        <DialogContent>
+          <TextField
+            autoFocus
+            margin="dense"
+            id="name"
+            label="Name"
+            type="text"
+            fullWidth
+            value={name}
+            autoComplete="off"
+            onChange={(e) => {
+              setName(e.target.value);
+            }}
+          />
+          <InputLabel id="modelLabel">Model</InputLabel>
+          <Select
+            labelId="modelLabel"
+            id="model"
+            type="text"
+            fullWidth
+            value={model}
+            autoComplete="on"
+            onChange={(e) => {
+              setModel(e.target.value);
+            }}
+          >
+            {modelList.map((entry) => (
+              <MenuItem key={entry} value={entry}>
+                <ListItemText primary={entry} />
+              </MenuItem>
+            ))}
+          </Select>
+          <InputLabel id="ch24Label">Channel 2.4GHz</InputLabel>
+          <Select
+            labelId="ch24Label"
+            id="24ch"
+            label="ch24Label"
+            type="text"
+            fullWidth
+            value={channel24G}
+            autoComplete="off"
+            onChange={(e) => {
+              setChannel24G(e.target.value);
+            }}
+          >
+            {ch24GhzList.map((entry) => (
+              <MenuItem key={entry} value={entry}>
+                <ListItemText primary={entry} />
+              </MenuItem>
+            ))}
+          </Select>
+          <InputLabel id="ch5Label">Channel 5GHz</InputLabel>
+          <Select
+            labelId="ch5Label"
+            id="5ch"
+            type="text"
+            fullWidth
+            value={channel5G}
+            autoComplete="off"
+            onChange={(e) => {
+              setChannel5G(e.target.value);
+            }}
+          >
+            {ch5GhzList.map((entry) => (
+              <MenuItem key={entry} value={entry}>
+                <ListItemText primary={entry} />
+              </MenuItem>
+            ))}
+          </Select>
+          <InputLabel id="switchLabel">Access Switch</InputLabel>
+          <Select
+            labelId="switchLabel"
+            id="accessSwitch"
+            type="text"
+            fullWidth
+            value={accessSwitch}
+            autoComplete="off"
+            onChange={(e) => {
+              setAccessSwitch(e.target.value);
+            }}
+          >
+            {props.switchList.map((entry) => (
+              <MenuItem key={entry.switchName} value={entry.switchName}>
+                <ListItemText primary={entry.switchName} />
+              </MenuItem>
+            ))}
+          </Select>
+        </DialogContent>
+      </Dialog>
+    </>
   );
 }
-
-export default DeviceEditBox;
